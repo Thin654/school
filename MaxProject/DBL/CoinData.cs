3b4baa1944674df5b7f16d278d2fafd30b64b03a
max <maxmaxsht@gmail.com>
2024-02-22 10:52:18 +0200

    220224

---
+++ MaxProject/DBL/CoinData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBL
+{
+    public class CoinData
+    {
+        public double usd { get; set; }
+        
+    }
+}
